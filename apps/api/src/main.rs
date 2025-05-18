64cb6832db29f0fbdb61afc40ec93dc96ceb66b4
Theo Bayenet <dev.js.theo@gmail.com>
2025-05-18 18:26:08 +0200

    Feature(Api): get video subtitles w/ local binary yt-dlp + return subtitles to client

--- apps/api/src/main.rs
+++ apps/api/src/main.rs
@@ -1,17 +1,18 @@
+mod helpers;
 mod routes;
 
 use axum::{
-    routing::{get},
+    routing::{post},
     Router,
 };
-use routes::decode::get_decoded_controller;
+use routes::decode::post_decode_handler;
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
     let app = Router::new()
-        .route("/api/v1/decode", get(get_decoded_controller));
+        .route("/api/v1/decode", post(post_decode_handler));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
--- apps/api/src/routes/decode.rs
+++ apps/api/src/routes/decode.rs
@@ -1,5 +1,36 @@
-use axum::{Json, http::StatusCode};
+use axum::{
+    http::{StatusCode, header, HeaderMap, HeaderValue},
+    Json,
+    response::IntoResponse,
+};
+use serde::Deserialize;
+use crate::helpers::download_subtitles::download_and_read_subtitles;
 
-pub async fn get_decoded_controller() -> (StatusCode, Json<String>) {
-    (StatusCode::OK, Json("Decoded data".to_string()))
+#[derive(Debug, Deserialize)]
+pub struct PostDecodeRequest {
+    youtube_url: String,
+    language: String,
+}
+
+pub async fn post_decode_handler(
+    Json(payload): Json<PostDecodeRequest>,
+) -> Result<impl IntoResponse, StatusCode> {
+    let output_prefix = "temp";
+
+    match download_and_read_subtitles(&payload.youtube_url, &payload.language, output_prefix).await {
+        Ok(subtitles) => {
+            // Create custom headers
+            let mut headers = HeaderMap::new();
+            headers.insert(
+                header::CONTENT_TYPE, 
+                HeaderValue::from_static("text/vtt")
+            );
+            
+            Ok((headers, subtitles))
+        },
+        Err(err) => {
+            eprintln!("Erreur: {}", err);
+            Err(StatusCode::BAD_REQUEST)
+        }
+    }
 }
